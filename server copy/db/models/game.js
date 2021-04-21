6ff5a6788ea52f4da0e14862cdcf3362c7932087
jerry valsin <jerryvalsin@gmail.com>
2021-04-21 19:52:22 +0200

    delete repo server copy

--- server copy/db/models/game.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose')
-
-const GameSchema = mongoose.Schema({
-  totalCashAmount: Number,
-  businesses: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Business' }],
-  lastEventDateInMs: Number
-})
-
-module.exports = mongoose.model('Game', GameSchema)
